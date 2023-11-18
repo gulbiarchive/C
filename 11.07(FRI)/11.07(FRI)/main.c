afd478281f0e8a278ee4ec9063661f747c201aa5
gulbi <123242543+gulbiarchive@users.noreply.github.com>
2023-11-18 17:42:49 +0900

    Add files via upload

---
+++ 11.07(FRI)/11.07(FRI)/main.c
@@ -0,0 +1,121 @@
+//
+//  main.c
+//  11.07(FRI)
+//
+//  Created by 박지현 on 2023/11/17.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+//    for(int i = 5; i >2; i--){
+//        printf("ABC");
+//    }
+//    printf("D");
+    
+//    int sum = 0;
+//
+//    for(int i = 2; i <=2019; i += 2){
+//        sum += i;
+//    }
+//    printf("1부터 2019까지 숫자 중 짝수의 합은 %d\n", sum);
+    
+//    for(int i = 1; i <= 100; i++){
+//        if(i % 4 == 0)
+//            printf("A\n");
+//        else if(i % 4 == 1)
+//            printf("B\n");
+//        else if(i % 4 == 2)
+//            printf("C\n");
+//        else
+//            printf("D\n");
+//    }
+    
+//    int x, y;
+//    int sum = 0;
+//    scanf("%d %d", &x, &y);
+//
+//    // 입력받은 x부터 y까지 반복
+//    for(int i = x; i <= y; i++){
+//        sum += i; // 누적 합 계산
+//    }
+//
+//    printf("%d\n", sum);
+    
+//    int N;
+//    scanf("%d", &N);
+//
+//    for(int i = N; i > 0; i--)
+//        printf("%d\n", i);
+    
+    
+    /* 곱셈을 연산해야 되므로 초기 값 0 안 됨
+    0을 곱하면 어떤 수라도 0이 되기 때문 */
+//    int n, result = 1;
+//    scanf("%d", &n);
+//
+//    for (int i = 1; i <= n; i++) {
+//        result *= i;
+//    }
+//
+//    printf("%d", result);
+    
+//    for(int i = 0; i < 10; i++){
+//        if(i == 5){
+//            continue;;
+//        }
+//
+//        printf("%d\n", i);
+//    }
+    /* 출력
+     0
+     1
+     2
+     3
+     4
+     6
+     7
+     8
+     9
+     */
+    
+//    for(int i = 100; i <= 500; i++){
+//        if(i == 200){
+//            continue;
+//        }
+//        printf("%d\n", i);
+//    }
+    
+//    for(int i = 0; i < 4; i++){
+//        for(int j = 0; j < 4; j++){
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+    
+//    for(int i = 2; i < 10; i++){
+//        if(i == 3){
+//            continue;
+//        }
+//        for(int j = 1; j < 10; j++){
+//            printf("%d x %d = %d\n", i, j, i * j);
+//        }
+//    }
+    
+//    int N;
+//    scanf("%d", &N);
+//
+//    for(int i = 0; i < N; i++){
+//        for(int j = 0; j <= i; j++){
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+    
+    
+    return 0;
+    
+}
+
+
